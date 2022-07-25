8283195df484637ba4271a49595c6927db2d9ddd
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-07-25 14:16:58 -0400

    test

---
+++ src/app/dashboard/todo-details/todo-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoDetailsComponent } from './todo-details.component';
+
+describe('TodoDetailsComponent', () => {
+  let component: TodoDetailsComponent;
+  let fixture: ComponentFixture<TodoDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  
+});
